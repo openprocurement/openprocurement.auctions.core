1276f79e72a2dec557eced287e5487cdcc780f3c
Andrew Leitsius <leits@quintagroup.com>
2018-01-10 15:21:40 +0200

    Fix submission_method_details test blanks

--- openprocurement/auctions/core/tests/blanks/auction_blanks.py
+++ openprocurement/auctions/core/tests/blanks/auction_blanks.py
@@ -5,19 +5,19 @@ def submission_method_details_no_auction(self):
     self.initial_data['submissionMethodDetails'] = u'quick(mode:no-auction)'
     self.create_auction()
     self.app.authorization = ('Basic', ('auction', ''))
-    result = self.app.post_json('/auctions/{}/auction'.format(self.auction_id),
-                                {'data': {'bids': self.initial_bids}})
-    self.assertEqual(result['auctionPeriod']['startDate'],
-                     result['auctionPeriod']['endDate'])
-    self.assertEqual(result['status'], "active.qualification")
+    auction = self.app.post_json('/auctions/{}/auction'.format(self.auction_id),
+                                {'data': {'bids': self.initial_bids}}).json['data']
+    self.assertEqual(auction['auctionPeriod']['startDate'],
+                     auction['auctionPeriod']['endDate'])
+    self.assertEqual(auction['status'], "active.qualification")
 
 
 def submission_method_details_fast_forward(self):
     self.initial_data['submissionMethodDetails'] = u'quick(mode:fast-forward)'
     self.create_auction()
     self.app.authorization = ('Basic', ('auction', ''))
-    result = self.app.post_json('/auctions/{}/auction'.format(self.auction_id),
-                                {'data': {'bids': self.initial_bids}})
-    self.assertEqual(result['auctionPeriod']['startDate'],
-                     result['auctionPeriod']['endDate'])
-    self.assertEqual(result['status'], "active.qualification")
+    auction = self.app.post_json('/auctions/{}/auction'.format(self.auction_id),
+                                {'data': {'bids': self.initial_bids}}).json['data']
+    self.assertEqual(auction['auctionPeriod']['startDate'],
+                     auction['auctionPeriod']['endDate'])
+    self.assertEqual(auction['status'], "active.qualification")
