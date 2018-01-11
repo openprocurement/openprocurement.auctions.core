585adc271e3c0ae3c6688a327faa0020a268b4fa
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-01-11 11:30:16 +0200

    Insert `Contract` model from flash Insert `Contract` model from dgf

---
+++ openprocurement/auctions/core/plugins/contracting/dgf/models.py
@@ -0,0 +1,28 @@
+from openprocurement.auctions.core.plugins.contracting.flash.models import (
+    Contract as BaseContract
+)
+from schematics.types.compound import ModelType
+from openprocurement.api.models import (
+    ListType,
+)
+from openprocurement.auctions.core.models import (
+    dgfOrganization as Organization,
+    dgfItem as Item,
+    dgfDocument as Document,
+    dgfComplaint as Complaint
+)
+from openprocurement.auctions.core.validation import (
+    validate_disallow_dgfPlatformLegalDetails
+)
+
+
+class Contract(BaseContract):
+    items = ListType(ModelType(Item))
+    suppliers = ListType(ModelType(Organization), min_size=1, max_size=1)
+    complaints = ListType(ModelType(Complaint), default=list())
+    documents = ListType(
+        ModelType(Document),
+        default=list(),
+        validators=[validate_disallow_dgfPlatformLegalDetails]
+    )
+
