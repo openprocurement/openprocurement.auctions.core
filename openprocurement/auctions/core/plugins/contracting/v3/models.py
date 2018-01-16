0fac36ae961a70ffa70d1e1a57fa523f37e24476
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-01-16 12:14:11 +0200

    Add fields `datePaid` and `signingPeriod`
    	- Refactor: rename variables; make code more clear
    	- Fix validation of `datePaid` of Contract-v3
    	- Add test for `signingPeriod`: Test checks copying of the field.
    	- Also make code-style refactor of view.

--- openprocurement/auctions/core/plugins/contracting/v3/models.py
+++ openprocurement/auctions/core/plugins/contracting/v3/models.py
@@ -1,13 +1,22 @@
+# -*- coding:utf-8 -*-
 from schematics.types.compound import ModelType
+from schematics.types.serializable import serializable
+from schematics.exceptions import ValidationError
+from schematics.transforms import (
+    blacklist,
+    whitelist
+)
 
 from openprocurement.api.models import (
     ListType,
+    Period,
+    IsoDateTimeType,
 )
 from openprocurement.auctions.core.models import (
     dgfOrganization as Organization,
     dgfItem as Item,
     dgfDocument as Document,
-    dgfComplaint as Complaint
+    dgfComplaint as Complaint,
 )
 from openprocurement.auctions.core.validation import (
     validate_disallow_dgfPlatformLegalDetails
@@ -21,9 +30,27 @@ class Contract(BaseContract):
     items = ListType(ModelType(Item))
     suppliers = ListType(ModelType(Organization), min_size=1, max_size=1)
     complaints = ListType(ModelType(Complaint), default=list())
+    signingPeriod = ModelType(Period)
+    datePaid = IsoDateTimeType()
     documents = ListType(
         ModelType(Document),
         default=list(),
         validators=[validate_disallow_dgfPlatformLegalDetails]
     )
+    class Options:
+        roles = {
+            'edit': blacklist('signingPeriod'),
+            'create': blacklist('signingPeriod', 'datePaid'),
+            'Administrator': whitelist('signingPeriod', 'datePaid'),
+        }
+
+    def validate_datePaid(self, data, value):
+        target_signingPeriod = data.get('signingPeriod')
+        if target_signingPeriod == None or \
+            value == None:
+            return None
+        if value < data.get('signingPeriod').get('startDate'):
+            raise ValidationError(
+                'datePaid must be less than start of signingPeriod'
+            )
 
