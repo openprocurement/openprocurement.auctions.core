c87a96056f108d6e1dff38070ba58cadeaea8982
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-02-07 12:44:18 +0200

    Add test for award periods

--- openprocurement/auctions/core/plugins/awarding/v3/tests/award.py
+++ openprocurement/auctions/core/plugins/awarding/v3/tests/award.py
@@ -19,6 +19,7 @@ from openprocurement.auctions.core.plugins.awarding.v3.tests.blanks.award_blanks
     create_auction_award,
     patch_auction_award_participant_disqualification,
     award_activation_creates_contract,
+    created_awards_have_periods_set,
 )
 
 
@@ -26,17 +27,37 @@ class AuctionAwardProcessTestMixin(object):
     test_invalid_patch_auction_award = snitch(invalid_patch_auction_award)
     test_patch_auction_award = snitch(patch_auction_award)
     test_patch_auction_award_admin = snitch(patch_auction_award_admin)
-    test_complate_auction_with_second_award1 = snitch(complate_auction_with_second_award1)
-    test_complate_auction_with_second_award2 = snitch(complate_auction_with_second_award2)
-    test_complate_auction_with_second_award3 = snitch(complate_auction_with_second_award3)
-    test_successful_second_auction_award = snitch(successful_second_auction_award)
+
+    test_complate_auction_with_second_award1 = snitch(
+        complate_auction_with_second_award1
+    )
+
+    test_complate_auction_with_second_award2 = snitch(
+        complate_auction_with_second_award2
+    )
+
+    test_complate_auction_with_second_award3 = snitch(
+        complate_auction_with_second_award3
+    )
+
+    test_successful_second_auction_award = snitch(
+        successful_second_auction_award
+    )
+
     test_unsuccessful_auction1 = snitch(unsuccessful_auction1)
     test_unsuccessful_auction2 = snitch(unsuccessful_auction2)
     test_unsuccessful_auction3 = snitch(unsuccessful_auction3)
     test_unsuccessful_auction4 = snitch(unsuccessful_auction4)
     test_unsuccessful_auction5 = snitch(unsuccessful_auction5)
     test_get_auction_awards = snitch(get_auction_awards)
-    test_patch_auction_award_participant_disqualification = snitch(patch_auction_award_participant_disqualification)
+
+    test_patch_auction_award_participant_disqualification = snitch(
+        patch_auction_award_participant_disqualification
+    )
+
+    test_created_awards_have_periods_set = snitch(
+        created_awards_have_periods_set
+    )
 
 
 class CreateAuctionAwardTestMixin(object):
