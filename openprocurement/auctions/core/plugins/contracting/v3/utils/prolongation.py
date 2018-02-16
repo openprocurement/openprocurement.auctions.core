812be163c301532a2f94dcb9737cbc8455a6a916
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-02-16 16:51:02 +0200

    Simplify conditional logic

--- openprocurement/auctions/core/plugins/contracting/v3/utils/prolongation.py
+++ openprocurement/auctions/core/plugins/contracting/v3/utils/prolongation.py
@@ -22,9 +22,12 @@ class ProlongationManager(object):
             p for p in self.prolongation.__parent__.prolongations
             if p.status == 'applied'
         ])
-        if applied_prolongations_count < 2:
-            self.prolongation.status = 'applied'
-            contract = self.prolongation.__parent__
+
+        if applied_prolongations_count >= 2:
+            raise Exception("Contract can be prolongated for 2 times only.")
+
+        self.prolongation.status = 'applied'
+        contract = self.prolongation.__parent__
         if applied_prolongations_count == 0:
             contract.signingPeriod.endDate = calculate_business_date(
                 contract.signingPeriod.startDate,
@@ -35,8 +38,6 @@ class ProlongationManager(object):
                 contract.signingPeriod.startDate,
                 PROLONGATION_LONG_PERIOD
             )
-        if applied_prolongations_count >= 2:
-            raise Exception("Contract can be prolongated for 2 times only.")
 
     def add_document(self, document):
         if self.prolongation.status == 'draft':
