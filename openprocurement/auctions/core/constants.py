4f701f52ae164d6f7dc566aff46b346d3cf210d1
Scandie <grizly.vl@gmail.com>
2018-02-22 14:43:39 +0200

    Add dict with statuses for validation
     * Statuses are dependent on procurementMethodTypes and resource types

--- openprocurement/auctions/core/constants.py
+++ openprocurement/auctions/core/constants.py
@@ -2,3 +2,38 @@
 
 ENGLISH_AUCTION_PROCUREMENT_METHOD_TYPES = ["belowThreshold", "dgfOtherAssets", "dgfFinancialAssets"]
 DUTCH_AUCTION_PROCUREMENT_METHOD_TYPES = ["dgfInsider"]
+
+PROCEDURE_STATUSES = {
+    "dgfInsider": {
+        "tender_period_statuses": [
+            "active.tendering", "active.auction", "active.auction.dutch",
+            "active.auction.sealedbid", "active.auction.bestbid"
+        ],
+        "auction_statuses": [
+            "active.auction", "active.auction.dutch",
+            "active.auction.sealedbid", "active.auction.bestbid"
+        ],
+        "bid_interaction_statuses": [
+            "active.tendering", "active.auction", "active.auction.dutch",
+            "active.auction.sealedbid", "active.auction.bestbid"
+        ],
+        "bid_doc_interaction_statuses": [
+            "active.tendering", "active.auction", "active.auction.dutch",
+            "active.auction.sealedbid", "active.auction.bestbid", "active.qualification"
+        ],
+        "complaint_statuses": [
+            "active.tendering", "active.auction", "active.auction.dutch",
+            "active.auction.sealedbid", "active.auction.bestbid",
+            "active.qualification", "active.awarded"
+        ]
+
+    },
+    "dgfOtherAssets": {
+        "tender_period_statuses": ["active.tendering", "active.auction"],
+        "auction_statuses": ["active.auction"],
+        "bid_interaction_statuses": ["active.tendering"],
+        "bid_doc_interaction_statuses": ["active.tendering", "active.qualification"],
+        "complaint_statuses": ["active.tendering", "active.auction", "active.qualification", "active.awarded"]
+    }
+}
+PROCEDURE_STATUSES["dgfFinancialAssets"] = PROCEDURE_STATUSES["dgfOtherAssets"]
