fd586b54a848418fe0ea312abdf9c45a1e7cef1e
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-03-14 12:20:40 +0200

    Move constants to core.plugins

---
+++ openprocurement/auctions/core/plugins/awarding/v2_1/constants.py
@@ -0,0 +1,9 @@
+from datetime import timedelta
+
+
+VERIFY_AUCTION_PROTOCOL_TIME = timedelta(days=6)
+AWARD_PAYMENT_TIME = timedelta(days=20)
+CONTRACT_SIGNING_TIME = timedelta(days=20)
+NUMBER_OF_BIDS_TO_BE_QUALIFIED = 2
+
+
