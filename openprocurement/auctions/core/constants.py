5a86ffbacf44b0d5540a60534f380137134c9188
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-03-15 16:30:46 +0200

    Move constants from cdb2 dgf to core

--- openprocurement/auctions/core/constants.py
+++ openprocurement/auctions/core/constants.py
@@ -1,6 +1,6 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 
-from openprocurement.api.models import ORA_CODES
+from openprocurement.api.models import ORA_CODES, TZ
 from openprocurement.auctions.core.utils import read_json
 
 DOCUMENT_TYPE_OFFLINE = ['x_dgfAssetFamiliarization']
@@ -11,6 +11,17 @@ ORA_CODES[0:0] = ["UA-IPN", "UA-FIN"]
 
 CAV_CODES_FLASH = read_json('cav_flash.json')
 CAV_CODES_DGF = read_json('cav_dgf.json')
+
+CAVPS_CODES_DGF_CDB2 = read_json('cav_ps_dgf_cdb2.json')
+CPVS_CODES_DGF_CDB2 = read_json('cpvs_dgf_cdb2.json')
+
+# CDB2 dgf
+CPV_NON_SPECIFIC_LOCATION_UNITS_DGF_CDB2 = ('45', '48', '50', '51', '55', '60', '63', '64',
+                                   '65', '66', '71', '72', '73', '75', '76', '77',
+                                   '79', '80', '85', '90', '92', '98')
+CAV_NON_SPECIFIC_LOCATION_UNITS_DGF_CDB2 = ('07', '08')
+DGF_CDB2_ADDRESS_REQUIRED_FROM = datetime(2018, 2, 9, tzinfo=TZ)
+DGF_CDB2_CLASSIFICATION_PRECISELY_FROM = datetime(2017, 7, 19, tzinfo=TZ)
 
 # Periods of prolongations, that can be applied on Contract
 PROLONGATION_SHORT_PERIOD = timedelta(days=42)
