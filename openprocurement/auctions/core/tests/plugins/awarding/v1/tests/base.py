d0cdd8943586864f0bc2ee2f2a9ae03b126b0f05
Scandie <grizly.vl@gmail.com>
2018-04-27 18:37:41 +0300

    Move test_organization, test_auction_data and base_test_bids to core

--- openprocurement/auctions/core/tests/plugins/awarding/v1/tests/base.py
+++ openprocurement/auctions/core/tests/plugins/awarding/v1/tests/base.py
@@ -1,21 +1,2 @@
 # -*- coding: utf-8 -*-
-
-test_organization = {
-    "name": u"Державне управління справами",
-    "identifier": {
-        "scheme": u"UA-EDR",
-        "id": u"00037256",
-        "uri": u"http://www.dus.gov.ua/"
-    },
-    "address": {
-        "countryName": u"Україна",
-        "postalCode": u"01220",
-        "region": u"м. Київ",
-        "locality": u"м. Київ",
-        "streetAddress": u"вул. Банкова, 11, корпус 1"
-    },
-    "contactPoint": {
-        "name": u"Державне управління справами",
-        "telephone": u"0440000000"
-    }
-}
+from openprocurement.auctions.core.tests.base import test_organization  # noqa forwarded import
