42c19756b0af86e7a642076ca015322fec11622a
Scandie <grizly.vl@gmail.com>
2018-05-09 16:52:33 +0300

    Add model forwarding imports

--- openprocurement/auctions/core/models.py
+++ openprocurement/auctions/core/models.py
@@ -60,7 +60,9 @@ from openprocurement.api.models.common import (
     Period,
     Guarantee,
     PeriodEndRequired as AuctionPeriodEndRequired,
-    Revision
+    Revision,
+    BankAccount,  # noqa forwarded import
+    AuctionParameters,  # noqa forwarded import
 )
 from openprocurement.api.models.schematics_extender import DecimalType
 from openprocurement.api.utils import get_now, get_request_from_root
