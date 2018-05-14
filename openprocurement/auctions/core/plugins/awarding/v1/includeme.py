e644edfa34cbdcba05d8a4b7632115a7fd104706
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-05-14 13:50:24 +0300

    Bring AwardingV1 manager adapter registration

--- openprocurement/auctions/core/plugins/awarding/v1/includeme.py
+++ openprocurement/auctions/core/plugins/awarding/v1/includeme.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
+from openprocurement.auctions.core.plugins.awarding.base.interfaces import (
+    IAwardManagerAdapter,
+)
 
 
 def includeme(config):
+    from .interfaces import IAwardV1
+    from .adapters import AwardManagerV1Adapter
     config.scan("openprocurement.auctions.core.plugins.awarding.v1.views")
+    config.registry.registerAdapter(
+        AwardManagerV1Adapter,
+        (IAwardV1, ),
+        IAwardManagerAdapter
+    )
