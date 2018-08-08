53798e7016416ad26120707a55e48722521ffeff
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-08-08 16:56:48 +0300

    Enable deprecation message

--- openprocurement/auctions/core/models/__init__.py
+++ openprocurement/auctions/core/models/__init__.py
@@ -5,6 +5,6 @@ from zope.deprecation import moved
 message = "openprocurement.auctions.core.models now contains submodules: schema, validators and roles. "
 "Please, re-import your stuff from there"
 
-moved('openprocurement.auctions.core.models.schema')
-moved('openprocurement.auctions.core.models.roles')
-moved('openprocurement.auctions.core.models.validators')
+moved('openprocurement.auctions.core.models.schema', message)
+moved('openprocurement.auctions.core.models.roles', message)
+moved('openprocurement.auctions.core.models.validators', message)
