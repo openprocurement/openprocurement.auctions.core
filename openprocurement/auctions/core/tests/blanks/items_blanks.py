1ed20ecab42a9277f03f4b9b73cbddf4df1ca43a
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-08-20 14:25:13 +0300

    Tests for dgfItem POST (failing)

--- openprocurement/auctions/core/tests/blanks/items_blanks.py
+++ openprocurement/auctions/core/tests/blanks/items_blanks.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from openprocurement.auctions.core.endpoints import ENDPOINTS
+from openprocurement.auctions.core.tests.helpers import (
+    get_auction,
+)
+from openprocurement.auctions.core.tests.fixtures.items import dgf_item
 
 
 def post_item(test_case):
-    pass
+    response = test_case.app.post_json(
+        ENDPOINTS['items'].format(auction_id=test_case.auction_id),
+        {'data': dgf_item}
+    )
+    test_case.assertEqual(response.status_code, 201)
