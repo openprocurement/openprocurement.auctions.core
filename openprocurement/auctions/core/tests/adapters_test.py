9c0aaf82ff8dc7f93d462cc632614e2510cf07a8
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-09-11 17:55:23 +0300

    Add pendify_auction function
    Move `pendify_auction_status` to base auction adapter

---
+++ openprocurement/auctions/core/tests/adapters_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from mock import Mock
+
+from openprocurement.auctions.core.adapters import (
+    AuctionManagerAdapter,
+)
+
+
+class AuctionManagerAdapterTest(unittest.TestCase):
+
+    def setUp(self):
+        self.auction = Mock()
+        self.adapter = AuctionManagerAdapter(self.auction)
+
+    def test_terminalize_with_merchandising_object(self):
+        self.auction.merchandisingObject = '1' * 32
+        self.adapter.pendify_auction_status('complete')
+        self.assertEqual(self.auction.status, 'pending.complete')
+
+    def test_terminalize_without_merchandising_object(self):
+        del self.auction.merchandisingObject
+        self.adapter.pendify_auction_status('complete')
+        self.assertEqual(self.auction.status, 'complete')
