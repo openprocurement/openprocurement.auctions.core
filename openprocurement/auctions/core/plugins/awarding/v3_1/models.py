7c65873ae3a831ff3276b5ab6eb196df4e2dbbaf
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-09-20 11:43:45 +0300

    Use enhanced calendar days calculation
    Make endDate always be working day.

--- openprocurement/auctions/core/plugins/awarding/v3_1/models.py
+++ openprocurement/auctions/core/plugins/awarding/v3_1/models.py
@@ -87,6 +87,11 @@ class Award(BaseAward):
         if not period.endDate:
             auction = get_auction(self)
             period.endDate = calculate_business_date(
-                period.startDate, CONTRACT_SIGNING_TIME, auction, working_days=False, specific_hour=AWARDING_PERIODS_END_DATE_HOUR
+                period.startDate,
+                CONTRACT_SIGNING_TIME,
+                auction,
+                working_days=False,
+                specific_hour=AWARDING_PERIODS_END_DATE_HOUR,
+                result_is_working_day=True
             )
         return period.to_primitive()
