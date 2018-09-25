aaeb46954adfc74604bb80726f977ec9cf8e3686
Andrew Leitsius <leits@quintagroup.com>
2018-09-25 14:56:21 +0300

    Revert Remove notice doc type #170

--- openprocurement/auctions/core/models/schema.py
+++ openprocurement/auctions/core/models/schema.py
@@ -475,6 +475,7 @@ class awardV3_1Document(swiftsureDocument):
 class contractV3_1Document(swiftsureDocument):
     documentType = StringType(
         choices=[
+            'notice',
             'contractSigned',
             'contractAnnexe',
             'rejectionProtocol',
