c99da53e3f3c10d9beb734b360e0c097030a7f79
Andrew Leitsius <leits@quintagroup.com>
2018-09-26 16:52:26 +0300

    Prepare openprocurement.auctions.core 1.2.6+patch1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.2.6'
+version = '1.2.6+patch1'
 auction_core = 'openprocurement.auctions.core.includeme:includeme'
 auction_transferring = 'openprocurement.auctions.core.plugins.transferring.includeme:includeme'
 
